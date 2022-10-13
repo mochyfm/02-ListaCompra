2ed47113b8261bfaa50252c862e3a8813358ff2d
Moisés Fernández Muiña <mochyfm@gmail.com>
2022-10-13 10:45:48 +0100

    Initial commit
    Innitial commit (Modified by Moisés)

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.mainBackground}>
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainBackground: {
+    flex: 1,
+    backgroundColor: '#6A0080',
+    width: '100%',
+    height: '100%'
+  }
+});
